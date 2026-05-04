618f230450001127b62f32b0953d5f42b4e7a34c
Esraa Ashraf <161146099+Esraa-ashraff@users.noreply.github.com>
2026-05-04 22:40:40 +0300

    Refactor comments in quality_checks_silver.sql
    Updated comments in quality_checks_silver.sql for clarity.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,7 +1,7 @@
 /*
-===============================================================================
+================================================================================
 Quality Checks
-===============================================================================
+================================================================================
 Script Purpose:
     This script performs various quality checks for data consistency, accuracy, 
     and standardization across the 'silver' layer. It includes checks for:
@@ -14,5 +14,5 @@ Script Purpose:
 Usage Notes:
     - Run these checks after data loading Silver Layer.
     - Investigate and resolve any discrepancies found during the checks.
-===============================================================================
+================================================================================
 */
