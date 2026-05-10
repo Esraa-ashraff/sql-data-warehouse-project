dff144959f2f42fddecc252953c4afb2ed1dc45c
Esraa Ashraf <161146099+Esraa-ashraff@users.noreply.github.com>
2026-05-10 06:56:55 +0300

    Refactor comments in quality_checks_silver.sql
    Updated comments and formatting in quality_checks_silver.sql for clarity and consistency.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,7 +1,7 @@
 /*
-=================================================================================
+========================================================================
 Quality Checks
-=================================================================================
+========================================================================
 Script Purpose:
     This script performs various quality checks for data consistency, accuracy, 
     and standardization across the 'silver' layer. It includes checks for:
@@ -14,6 +14,28 @@ Script Purpose:
 Usage Notes:
     - Run these checks after data loading Silver Layer.
     - Investigate and resolve any discrepancies found during the checks.
-=================================================================================
+=======================================================================
 
 */
+
+-- ====================================================================
+-- Checking 'silver.crm_cust_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT cst_id,COUNT (*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*)>1 OR cst_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+
